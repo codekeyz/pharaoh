2358f44738273a0a796c0022d610e4085b584a97
Chima Precious <ositachima.co@gmail.com>
2023-11-12 02:06:23 +0000

    Return new instance of RouteHandler when prefixed

--- lib/src/router/handler.dart
+++ lib/src/router/handler.dart
@@ -13,11 +13,15 @@ typedef ProcessHandlerFunc = (ReqRes data, HandlerFunc handler);
 ///
 /// This way you can reprocess contents in the
 /// request before it reaches other handlers in your application.
+///
+/// See here: [Middleware]
 typedef HandlerFunc = FutureOr<dynamic> Function(Request req, Response res);
 
 /// This type of handler uses the Request interface [$Request]
 /// which is nothing but an interface. All you have on this are getter calls
 /// to get information about Requests reaching your application
+///
+/// See here: [RequestHandler]
 typedef RequestHandlerFunc = FutureOr<dynamic> Function(
   $Request req,
   Response res,
@@ -26,20 +30,40 @@ typedef RequestHandlerFunc = FutureOr<dynamic> Function(
 /// All route handler types must extend this class.
 ///
 /// See: [RequestHandler] and [Middleware] types
-abstract class RouteHandler {
-  Route route;
-  final HandlerFunc handler;
-  RouteHandler(this.handler, this.route);
-  RouteHandler prefix(String prefix) {
-    route = route.withPrefix(prefix);
-    return this;
-  }
+abstract interface class RouteHandler {
+  Route get route;
+  HandlerFunc get handler;
+  RouteHandler prefix(String prefix);
 }
 
-class RequestHandler extends RouteHandler {
-  RequestHandler(RequestHandlerFunc func, Route route) : super(func, route);
+class RequestHandler implements RouteHandler {
+  final RequestHandlerFunc _func;
+  final Route _route;
+  const RequestHandler(this._func, this._route);
+
+  @override
+  RequestHandler prefix(String prefix) =>
+      RequestHandler(_func, route.withPrefix(prefix));
+
+  @override
+  HandlerFunc get handler => _func;
+
+  @override
+  Route get route => _route;
 }
 
-class Middleware extends RouteHandler {
-  Middleware(super.handler, super.route);
+class Middleware implements RouteHandler {
+  final HandlerFunc _func;
+  final Route _route;
+  const Middleware(this._func, this._route);
+
+  @override
+  Middleware prefix(String prefix) =>
+      Middleware(_func, route.withPrefix(prefix));
+
+  @override
+  HandlerFunc get handler => _func;
+
+  @override
+  Route get route => _route;
 }
