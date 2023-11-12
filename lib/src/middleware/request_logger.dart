ccb5e7c0c3f0df0c7db64c3eb8eb135bcc324948
Chima Precious <ositachima.co@gmail.com>
2023-11-12 03:54:06 +0000

    Add basic request logger to example

--- lib/src/middleware/request_logger.dart
+++ lib/src/middleware/request_logger.dart
@@ -1,11 +1,16 @@
 import '../http/request.dart';
 import '../http/response.dart';
 import '../router/handler.dart';
-import '../router/route.dart';
 
 Future<ReqRes> _processBody(Request req, Response _) async {
+  final logLines = """
+-------------------------------------------------------
+Path:             ${req.path}
+Params:           ${req.method.name}
+Content-Type      ${req.mimeType}
+-------------------------------------------------------\n""";
+  print(logLines);
   return (req, _);
 }
 
-Middleware logRequests([Route? route]) =>
-    Middleware(_processBody, route ?? Route.any());
+HandlerFunc logRequests = _processBody;
