bada8752ead81a393825bc1ea2a11fc73ab7b93a
Chima Precious <ositachima.co@gmail.com>
2023-11-12 02:07:43 +0000

    Use new instance of handler

--- lib/src/router/route.dart
+++ lib/src/router/route.dart
@@ -45,17 +45,15 @@ class RouteGroup {
   RouteGroup(this.prefix);
 
   void add(RouteHandler handler) {
-    var route = handler.route;
-
-    if (route.route.trim().isEmpty) {
+    if (handler.route.route.trim().isEmpty) {
       throw Exception('Routes should being with $BASE_PATH');
     }
 
     if (![BASE_PATH, ANY_PATH].contains(prefix)) {
-      handler.prefix(prefix);
+      handler = handler.prefix(prefix);
     }
 
-    /// TODO: do checks here to make sure there's no duplicate entry in the routes
+    /// TODO(codekeyz) do checks here to make sure there's no duplicate entry in the routes
     handlers.add(handler);
   }
 
