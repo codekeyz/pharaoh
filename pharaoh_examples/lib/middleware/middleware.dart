e08d73a49b0219c562f2159c0c188e6bc4d16d76
Chima Precious <ositachima.co@gmail.com>
2023-11-16 00:55:40 +0000

    Fix/pub dev score (#21)
    * Rename examples folder
    
    * Do not ignore tests folder
    
    * Update example usages
    
    * Update example
    
    * Bump package version
    
    * Remove shelf middleware example

---
+++ pharaoh_examples/lib/middleware/middleware.dart
@@ -0,0 +1,11 @@
+import 'package:pharaoh/pharaoh.dart';
+
+final app = Pharaoh();
+
+void main() async {
+  app.use(logRequests);
+
+  app.get('/', (req, res) => res.ok("Hurray 🚀"));
+
+  await app.listen();
+}
