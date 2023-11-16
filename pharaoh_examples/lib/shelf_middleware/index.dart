f29c9b8bc93b9f4d6f7165988fa776cc4da27585
Chima Precious <ositachima.co@gmail.com>
2023-11-16 01:00:34 +0000

    Add shelf middleware example

---
+++ pharaoh_examples/lib/shelf_middleware/index.dart
@@ -0,0 +1,13 @@
+import 'package:pharaoh/pharaoh.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
+
+final app = Pharaoh();
+
+void main() async {
+  /// Using shelf_cors_header with Pharaoh
+  app.use(useShelfMiddleware(corsHeaders()));
+
+  app.get('/', (req, res) => res.json(req.headers));
+
+  await app.listen();
+}
