99be142bb63fdf4ff5b281f46560ba619417b974
Chima Precious <ositachima.co@gmail.com>
2023-11-16 23:59:07 +0000

    Add converters for the two Type signatures of shelf middlewares i know
    If in any case, you ran into an issue of a `Shelf Middleware` not working fine with
    `Pharaoah`, please @me on github. I'll look into it asap. Or you can just loop at the code
    in the adapter, add a case for that `Type` and submit a PR

--- lib/src/shelf_interop/adapter.dart
+++ lib/src/shelf_interop/adapter.dart
@@ -1,21 +1,42 @@
+import 'dart:async';
+
 import '../http/request.dart';
 import '../http/response.dart';
 import '../router/handler.dart';
+import '../utils/exceptions.dart';
 import 'shelf.dart' as shelf;
 
-extension _ShelfResponseMixin on shelf.Response {
-  /// TODO(codekeyz)
-  ///
-  /// Investigate more on how the Shelf Middlewares
-  /// actually work. That way we will know when a Shelf middleware
-  /// allows us to continue execution or has terminated the request
-  /// lifecyle.
-  Response copyTo(Response res) {
-    return res
-      ..status(statusCode)
-      ..body = shelf.Body(read())
-      ..updateHeaders((hders) => hders..addAll(headers));
+typedef ShelfMiddlewareType2 = FutureOr<shelf.Response> Function(shelf.Request);
+
+/// Use this hook to transform any shelf
+/// middleware into a [HandlerFunc] that Pharaoh
+/// can use.
+///
+/// This will also throw an Exception if you use a Middleware
+/// that has a [Type] signature different from either [shelf.Middleware]
+/// or [ShelfMiddlewareType2] tho in most cases, it should work.
+HandlerFunc useShelfMiddleware(dynamic middleware) {
+  if (middleware is shelf.Middleware) {
+    return (req, res, next) async {
+      final shelfResponse = await middleware(
+          (req) => shelf.Response.ok(req.read()))(_toShelfRequest(req));
+
+      res = _fromShelfResponse(req, shelfResponse);
+
+      next(res);
+    };
   }
+
+  if (middleware is ShelfMiddlewareType2) {
+    return (req, res, next) async {
+      final shelfResponse = await middleware(_toShelfRequest(req));
+      res = _fromShelfResponse(req, shelfResponse);
+
+      next(res);
+    };
+  }
+
+  throw PharaohException.value('Unknown Shelf Middleware Type', middleware);
 }
 
 shelf.Request _toShelfRequest($Request req) {
@@ -36,12 +57,10 @@ shelf.Request _toShelfRequest($Request req) {
   );
 }
 
-HandlerFunc useShelfMiddleware(shelf.Middleware middleware) {
-  return (Request req, Response res, Function next) async {
-    final shelfResponse = await middleware(
-        (req) => shelf.Response.ok(req.read()))(_toShelfRequest(req));
-    res = shelfResponse.copyTo(res);
-
-    next(res);
-  };
+Response _fromShelfResponse(Request req, shelf.Response response) {
+  return Response.from(req.req, body: shelf.Body(response.read()))
+      .status(response.statusCode)
+      .updateHeaders(
+          (hdrs) => response.headers.forEach((key, value) => hdrs[key] = value))
+      .end();
 }
