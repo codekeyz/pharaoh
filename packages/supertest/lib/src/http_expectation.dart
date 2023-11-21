b8ac50cce5e1d242279f2527885f75a337f4dc5e
Chima Precious <ositachima.co@gmail.com>
2023-11-21 02:03:38 +0000

    Make supertest interface simpler

---
+++ packages/supertest/lib/src/http_expectation.dart
@@ -0,0 +1,60 @@
+import 'package:test/test.dart';
+import 'package:http/http.dart' as http;
+
+import 'expectation.dart';
+
+typedef HttpFutureResponse = Future<http.Response>;
+
+HttpResponseExpection expectHttp(HttpFutureResponse value) =>
+    HttpResponseExpection(value);
+
+typedef GetValueFromResponse<T> = T Function(http.Response response);
+
+typedef MatchCase = ({GetValueFromResponse value, dynamic matcher});
+
+class HttpResponseExpection
+    extends ExpectationBase<HttpFutureResponse, http.Response> {
+  HttpResponseExpection(HttpFutureResponse value) : super(value);
+
+  final List<MatchCase> _matchcases = [];
+
+  HttpResponseExpection header(String header, dynamic matcher) {
+    final MatchCase test =
+        (value: (resp) => resp.headers[header], matcher: matcher);
+    _matchcases.add(test);
+    return this;
+  }
+
+  HttpResponseExpection headers(dynamic matcher) {
+    final MatchCase test = (value: (resp) => resp.headers, matcher: matcher);
+    _matchcases.add(test);
+    return this;
+  }
+
+  HttpResponseExpection status(dynamic matcher) {
+    final MatchCase test = (value: (resp) => resp.statusCode, matcher: matcher);
+    _matchcases.add(test);
+    return this;
+  }
+
+  HttpResponseExpection body(dynamic matcher) {
+    final MatchCase value = (value: (resp) => resp.body, matcher: matcher);
+    _matchcases.add(value);
+    return this;
+  }
+
+  HttpResponseExpection custom(GetValueFromResponse check, Matcher matcher) {
+    final MatchCase value = (value: check, matcher: matcher);
+    _matchcases.add(value);
+    return this;
+  }
+
+  @override
+  Future<void> test() async {
+    final response = await actual;
+    // ignore: no_leading_underscores_for_local_identifiers
+    for (final _case in _matchcases) {
+      expect(_case.value(response), _case.matcher);
+    }
+  }
+}
--- packages/supertest/test/supertest_test.dart
+++ packages/supertest/test/supertest_test.dart
@@ -6,18 +6,17 @@ void main() {
   group("supertest", () {
     test('should fire up the app on an ephemeral port', () async {
       final app = Pharaoh().get('/', (req, res) => res.send('Hello World'));
-      final result = await (await (request<Pharaoh>(app))).get('/');
-      expect(result.statusCode, 200);
-      expect(result.body, 'Hello World');
+      await (await (request<Pharaoh>(app)))
+          .get('/')
+          .status(200)
+          .body('Hello World')
+          .test();
     });
 
     test('should work with an active server', () async {
       final app = Pharaoh().post('/hello', (req, res) => res.ok('Hello World'));
-      var result = await (await (request<Pharaoh>(app))).get('/');
-      expect(result.statusCode, 404);
-
-      result = await (await (request<Pharaoh>(app))).post('/hello');
-      expect(result.statusCode, 200);
+      await (await (request<Pharaoh>(app))).get('/').status(404).test();
+      await (await (request<Pharaoh>(app))).post('/hello').status(200).test();
     });
 
     test('should work with remote server', () async {
@@ -26,9 +25,13 @@ void main() {
 
       await app.listen();
 
-      var result = await (await (request<Pharaoh>(app))).put('/hello');
-      expect(result.statusCode, 200);
-      expect(result.body, 'Hey Daddy Yo!');
+      await (await (request<Pharaoh>(app)))
+          .put('/hello')
+          .status(200)
+          .body('Hey Daddy Yo!')
+          .test();
+
+      await app.shutdown();
     });
   });
 }
