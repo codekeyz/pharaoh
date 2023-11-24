06d28b574d5d351868c378b987a303173ac219c6
Chima Precious <ositachima.co@gmail.com>
2023-11-24 15:45:43 +0000

    improve tests

--- packages/pharaoh/test/middleware/cookie_parser_test.dart
+++ packages/pharaoh/test/middleware/cookie_parser_test.dart
@@ -12,15 +12,15 @@ void main() {
         final app = Pharaoh().use(cookieParser()).get(
           '/',
           (req, res) {
-            final cookies = req.cookies.map((e) => e.toString()).toList();
-            return res.json(cookies);
+            final str = req.cookies.toString();
+            return res.ok(str);
           },
         );
 
         await (await request(app))
             .get('/', headers: {HttpHeaders.cookieHeader: 'foo=bar; bar=baz'})
             .expectStatus(200)
-            .expectBody('["foo=bar; HttpOnly","bar=baz; HttpOnly"]')
+            .expectBody('[foo=bar; HttpOnly, bar=baz; HttpOnly]')
             .test();
       });
 
@@ -29,18 +29,18 @@ void main() {
         final app = Pharaoh().use(cookieParser(opts: opts)).get(
           '/',
           (req, res) {
-            final cookies = req.signedCookies.map((e) => e.toString()).toList();
-            return res.json(cookies);
+            final str = req.signedCookies.toString();
+            return res.ok(str);
           },
         );
 
         await (await request(app))
             .get('/', headers: {
               HttpHeaders.cookieHeader:
-                  'name=s%3Achima.4ytL9j25i8e59B6eCUUZdrWHdGLK3Cua%2BG1oGyurzXY; Path=/'
+                  'name=s%3Achima.4ytL9j25i8e59B6eCUUZdrWHdGLK3Cua%2BG1oGyurzXY'
             })
             .expectStatus(200)
-            .expectBody('["name=chima; HttpOnly"]')
+            .expectBody('[name=chima; HttpOnly]')
             .test();
       });
 
@@ -49,8 +49,8 @@ void main() {
         final app = Pharaoh().use(cookieParser(opts: opts)).get(
           '/',
           (req, res) {
-            final cookies = req.signedCookies.map((e) => e.toString()).toList();
-            return res.json(cookies);
+            final str = req.signedCookies.toString();
+            return res.ok(str);
           },
         );
 
@@ -69,15 +69,15 @@ void main() {
         final app = Pharaoh().use(cookieParser(opts: opts)).get(
           '/',
           (req, res) {
-            final cookies = req.cookies.map((e) => e.toString()).toList();
-            return res.json(cookies);
+            final str = req.cookies.toString();
+            return res.ok(str);
           },
         );
 
         await (await request(app))
             .get('/', headers: {HttpHeaders.cookieHeader: 'name=chima; Path=/'})
             .expectStatus(200)
-            .expectBody('["name=chima; HttpOnly","Path=/; HttpOnly"]')
+            .expectBody('[name=chima; HttpOnly, Path=/; HttpOnly]')
             .test();
       });
     });
@@ -87,8 +87,8 @@ void main() {
         final app = Pharaoh().use(cookieParser()).get(
           '/',
           (req, res) {
-            final cookies = req.cookies.map((e) => e.toString()).toList();
-            return res.json(cookies);
+            final str = req.cookies.toString();
+            return res.ok(str);
           },
         );
 
@@ -103,8 +103,8 @@ void main() {
         final app = Pharaoh().use(cookieParser()).get(
           '/',
           (req, res) {
-            final cookies = req.signedCookies.map((e) => e.toString()).toList();
-            return res.json(cookies);
+            final str = req.signedCookies.toString();
+            return res.ok(str);
           },
         );
 
