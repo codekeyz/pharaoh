f425cb7c4a3e69299fb4ef7d4c7f809f983ff595
Chima Precious <ositachima.co@gmail.com>
2023-11-25 18:09:10 +0000

    improve session

--- packages/pharaoh/lib/src/http/session.dart
+++ packages/pharaoh/lib/src/http/session.dart
@@ -1,30 +1,28 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 class Session {
   static const String name = 'pharaoh.sid';
 
   final String id;
-  final DateTime? expiry;
-  final int? originalMaxAge;
-
   final SessionStore _store;
+
+  Cookie? cookie;
 
   Map<String, dynamic>? _dataBag;
 
   Session(
     this.id, {
-    Duration? maxAge,
     required SessionStore store,
-  })  : _store = store,
-        originalMaxAge = maxAge?.inSeconds,
-        expiry = maxAge == null ? null : DateTime.now().add(maxAge);
+  }) : _store = store;
 
   Map<String, dynamic> toJson() => {
         'id': id,
-        'expiry': expiry?.toIso8601String(),
-        'originalMaxAge': originalMaxAge,
         'databag': _dataBag,
+        'cookie': cookie?.toString(),
+        'expiry': expiry?.toIso8601String(),
+        'originalMaxAge': cookie?.maxAge,
       };
 
   void operator []=(String name, dynamic value) {
@@ -41,11 +39,22 @@ class Session {
 
   FutureOr<void> destroy() => _store.destroy(id);
 
+  FutureOr<void> resetMaxAge() async {
+    final cookie_ = cookie;
+    if (cookie_ == null || cookie_.maxAge == null) return;
+
+    final expires = DateTime.now().add(Duration(seconds: cookie_.maxAge!));
+    cookie = cookie_..expires = expires;
+    return save();
+  }
+
   bool get valid {
     final exp = expiry;
     if (exp == null) return true;
     return exp.isAfter(DateTime.now());
   }
+
+  DateTime? get expiry => cookie?.expires;
 }
 
 abstract interface class SessionStore {
--- packages/pharaoh/lib/src/middleware/session.dart
+++ packages/pharaoh/lib/src/middleware/session.dart
@@ -12,16 +12,24 @@ typedef GenSessionIdFunc = FutureOr<String> Function(Request request);
 
 HandlerFunc session({
   String name = Session.name,
-  GenSessionIdFunc? generateId,
+  bool saveUninitialized = false,
+  bool resave = true,
+
+  /// The expiration is reset to the original maxAge,
+  /// resetting the expiration countdown and forces
+  /// the session identifier cookie to be set on every response.
+  bool rolling = false,
+  GenSessionIdFunc? genId,
   SessionStore? store,
-  required CookieOpts cookieOpts,
+  required CookieOpts cookie,
 }) {
-  final opts = cookieOpts..validate();
+  final opts = cookie..validate();
   final sessionStore = store ??= InMemoryStore();
   final uuid = Uuid();
 
   return (req, res, next) async {
-    void nextWithSession(Session session) {
+    void nextWithSession(Session session, {bool attachCookie = false}) async {
+      if (attachCookie) res = res.withCookie(session.cookie!);
       req[RequestContext.session] = session;
       req[RequestContext.sessionId] = session.id;
       return next((req: req, res: res));
@@ -32,21 +40,31 @@ HandlerFunc session({
 
     final req_sid =
         req.signedCookies.firstWhereOrNull((e) => e.name == name)?.value;
+
     if (req_sid != null) {
-      final session = await sessionStore.get(req_sid);
-      if (session != null && session.valid) {
-        return nextWithSession(session);
+      final s_ = await sessionStore.get(req_sid);
+      if (s_ != null && s_.valid) {
+        if (rolling) {
+          final rolled = bakeCookie(name, req_sid, opts);
+          s_.cookie = rolled;
+          await sessionStore.set(req_sid, s_);
+        }
+
+        return nextWithSession(s_, attachCookie: rolling);
       }
+
       await sessionStore.destroy(req_sid);
     }
 
-    final sessionId = await generateId?.call(req) ?? uuid.v4();
-    final session =
-        Session(sessionId, store: sessionStore, maxAge: cookieOpts.maxAge);
-    await sessionStore.set(sessionId, session);
+    final sessionId = await genId?.call(req) ?? uuid.v4();
+    final session = Session(sessionId, store: sessionStore);
+    final cookie = bakeCookie(name, sessionId, opts);
+    session.cookie = cookie;
 
-    /// add cookie to response and put session in context
-    res = res.cookie(name, sessionId, opts);
-    nextWithSession(session);
+    if (saveUninitialized) {
+      await sessionStore.set(sessionId, session);
+    }
+
+    return nextWithSession(session, attachCookie: true);
   };
 }
