0e12201f2f238c339804a7488a24244704934e52
Chima Precious <ositachima.co@gmail.com>
2023-11-26 14:14:49 +0000

    add tests for middleware chaining

--- packages/pharaoh/test/router/handler_test.dart
+++ packages/pharaoh/test/router/handler_test.dart
@@ -1,3 +1,4 @@
+import 'package:pharaoh/pharaoh.dart';
 import 'package:pharaoh/src/core.dart';
 import 'package:pharaoh/src/http/request.dart';
 import 'package:spookie/spookie.dart';
@@ -64,6 +65,52 @@ void main() {
           .expectStatus(200)
           .expectBody('Hello World')
           .test();
+    });
+
+    test('should chain middlewares in the right order', () async {
+      final listResultList = <int>[];
+
+      final HandlerFunc mdw1 = (req, res, next) {
+        listResultList.add(1);
+        next();
+      };
+
+      final HandlerFunc mdw2 = (req, res, next) {
+        listResultList.add(2);
+        next();
+      };
+
+      final HandlerFunc mdw3 = (req, res, next) {
+        listResultList.add(3);
+        next(res.end());
+      };
+
+      Pharaoh getApp(HandlerFunc chain) {
+        final app = Pharaoh();
+        return app.use(chain).get('/test', (req, res) => res.ok());
+      }
+
+      final testChain1 = mdw1.chain(mdw2).chain(mdw3);
+      await (await request(getApp(testChain1))).get('/test').test();
+      expect(listResultList, [1, 2, 3]);
+
+      listResultList.clear();
+
+      final testChain2 = mdw2.chain(mdw1).chain(mdw3);
+      await (await request(getApp(testChain2))).get('/test').test();
+      expect(listResultList, [2, 1, 3]);
+
+      listResultList.clear();
+
+      final testChain3 = mdw3.chain(mdw1.chain(mdw3)).chain(mdw2.chain(mdw1));
+      await (await request(getApp(testChain3))).get('/test').test();
+      expect(listResultList, [3, 1, 3, 2, 1]);
+
+      listResultList.clear();
+
+      final complexChain = testChain3.chain(testChain1).chain(testChain2);
+      await (await request(getApp(complexChain))).get('/test').test();
+      expect(listResultList, [3, 1, 3, 2, 1, 1, 2, 3, 2, 1, 3]);
     });
   });
 }
