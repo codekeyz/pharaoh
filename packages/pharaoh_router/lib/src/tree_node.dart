e46274bca55c7847b8f25dd35675f969c96093a5
Chima Precious <ositachima.co@gmail.com>
2023-11-28 03:26:24 +0000

    clean up unused funcs & methods

--- packages/pharaoh_router/lib/src/tree_node.dart
+++ packages/pharaoh_router/lib/src/tree_node.dart
@@ -10,17 +10,6 @@ class Node<T> {
   Node getChild(String char) => children[char]!;
 
   bool get hasChildren => children.isNotEmpty;
-
-  void addNode(String char, Node node) => children[char] = node;
-
-  Node? search(String path) {
-    Node root = this;
-    for (int i = 0; i < path.length; i++) {
-      final char = path[i];
-      if (root.hasChild(char)) root = root.children[char]!;
-    }
-    return root.terminal ? root : null;
-  }
 }
 
 class ParametricNode extends Node<Map<String, dynamic>> {
