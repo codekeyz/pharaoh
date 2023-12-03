ec820bd9476dc52b89b155ddfe1e0409c007034d
Chima Precious <ositachima.co@gmail.com>
2023-12-03 12:43:27 +0000

    tiny fix

--- packages/spanner/lib/src/route/action.dart
+++ packages/spanner/lib/src/route/action.dart
@@ -7,27 +7,41 @@ class RouteAction extends Equatable {
 
   const RouteAction(this.handler, {required this.method});
 
-  bool matches(Request request) =>
-      method == HTTPMethod.ALL || request.method == method;
-
   @override
   List<Object?> get props => [method];
 }
 
+typedef IndexedHandler = ({int index, RouteHandler hdler});
+
 mixin RouteActionMixin {
-  final Map<HTTPMethod, List<RouteHandler>> methodActionsMap = {};
+  final Map<HTTPMethod, List<IndexedHandler>> methodActionsMap = {};
 
   Iterable<HTTPMethod> get methods => methodActionsMap.keys;
 
   bool hasMethod(HTTPMethod method) => methodActionsMap.containsKey(method);
 
-  List<RouteHandler> getActions(HTTPMethod method) =>
-      methodActionsMap[method] ?? [];
+  int _currentIndex = 0;
+
+  List<RouteHandler> getActions(HTTPMethod method) {
+    if (methodActionsMap.isEmpty) return [];
+
+    final hdlersForMethod = methodActionsMap[method] ?? [];
+    final allHandlers = methodActionsMap[HTTPMethod.ALL] ?? [];
+
+    /// sorting is done to ensure we maintain the order in-which handlers
+    /// where added.
+    final result = [
+      if (allHandlers.isNotEmpty) ...allHandlers,
+      if (hdlersForMethod.isNotEmpty) ...hdlersForMethod,
+    ]..sort((a, b) => a.index.compareTo(b.index));
+
+    return result.map((e) => e.hdler).toList();
+  }
 
   void addAction(RouteAction action) {
     final method = action.method;
     final actionsList = methodActionsMap[method] ?? [];
-    actionsList.add(action.handler);
+    actionsList.add((index: _currentIndex++, hdler: action.handler));
     methodActionsMap[method] = actionsList;
   }
 }
