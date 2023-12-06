8340c39ce3d8dbc4f2e4aebe9ae14408964b77cb
Chima Precious <ositachima.co@gmail.com>
2023-12-06 20:27:18 +0000

    fix links to examplse

--- pharaoh_examples/lib/route_groups/index.dart
+++ pharaoh_examples/lib/route_groups/index.dart
@@ -3,17 +3,19 @@ import 'package:pharaoh/pharaoh.dart';
 final app = Pharaoh();
 
 void main() async {
-  final guestRouter = app.router()
-    ..get('/foo', (req, res) => res.ok("Hello World"))
-    ..post('/bar', (req, res) => res.json({"mee": "moo"}))
-    ..put('/yoo', (req, res) => res.json({"pookey": "reyrey"}));
+  final guestRouter = app
+      .router()
+      .get('/foo', (req, res) => res.ok("Hello World"))
+      .post('/bar', (req, res) => res.json({"mee": "moo"}))
+      .put('/yoo', (req, res) => res.json({"pookey": "reyrey"}));
 
-  final adminRouter = app.router()
-    ..use(logRequests)
-    ..get('/user', (req, res) => res.json({"chima": "happy"}))
-    ..put('/hello', (req, res) => res.json({"name": "chima"}))
-    ..post('/say-hello', (req, res) => res.notFound())
-    ..delete('/delete', (req, res) => res.json(req.body));
+  final adminRouter = app
+      .router()
+      .use(logRequests)
+      .get('/user', (req, res) => res.json({"chima": "happy"}))
+      .put('/hello', (req, res) => res.json({"name": "chima"}))
+      .post('/say-hello', (req, res) => res.notFound())
+      .delete('/delete', (req, res) => res.json(req.body));
 
   app.group('/admin', adminRouter);
 
