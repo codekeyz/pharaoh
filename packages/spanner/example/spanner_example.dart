4cd0216151e18fea5a46eb352f016c2049f53c54
Chima Precious <ositachima.co@gmail.com>
2023-12-11 11:59:54 +0000

    add spanner example with httpserver

--- packages/spanner/example/spanner_example.dart
+++ packages/spanner/example/spanner_example.dart
@@ -1,29 +1,55 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:spanner/spanner.dart';
 
-void main() {
+typedef Handler = String Function(Map<String, dynamic> params);
+
+void main() async {
   final spanner = Spanner();
 
-  List<String> getUsers() => ['Foo', 'Bar'];
+  getUsers(Map<String, dynamic> params) => jsonEncode(['Foo', 'Bar']);
 
-  String getUser(String userId) => 'Hello $userId';
+  getUser(Map<String, dynamic> params) => 'Hello ${params['userId']}';
 
-  spanner.addRoute(HTTPMethod.GET, '/', getUsers);
+  spanner
+    ..addRoute(HTTPMethod.GET, '/', getUsers)
+    ..addRoute(HTTPMethod.GET, '/<userId>', getUser);
 
-  spanner.addRoute(HTTPMethod.GET, '/<userId>', getUser);
+  final server = await HttpServer.bind('localhost', 0);
 
-  final result = spanner.lookup(HTTPMethod.GET, '/');
-  if (result == null) return;
+  print('Server Started on port: ${server.port}');
 
-  final routeParams = result.params; // Map<String, dynamic>
-  print(routeParams);
+  server.listen((request) {
+    if (request.method != 'GET') {
+      request.response
+        ..write('Request not supported')
+        ..close();
+      return;
+    }
 
-  /// your handler will be in this list.
-  ///
-  /// If any middlewares where resolved along the route to this handler
-  /// they'll be present in the list
-  ///
-  /// The list is ordered in the exact way you registed your middlewares
-  /// and handlers
-  final resolvedHandler = result.values; // List<dynamic>
-  print(resolvedHandler);
+    final result = spanner.lookup(HTTPMethod.GET, request.uri.path);
+    if (result == null) {
+      request.response
+        ..write('Request not supported')
+        ..close();
+      return;
+    }
+
+    final params = result.params; // Map<String, dynamic>
+
+    /// your handler will be in this list.
+    ///
+    /// If any middlewares where resolved along the route to this handler
+    /// they'll be present in the list
+    ///
+    /// The list is ordered in the exact way you registed your middlewares
+    /// and handlers
+    final resolvedHandler = result.values; // List<dynamic>
+
+    final handlerResult = (resolvedHandler.first as Handler).call(result.params);
+    request.response
+      ..write(handlerResult)
+      ..close();
+  });
 }
