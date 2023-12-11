9e470c6b11c645f99dd1be84f1b7157a156d2f39
Chima Precious <ositachima.co@gmail.com>
2023-12-11 12:19:35 +0000

    chore: update spanner version (#85)
    * update spanner version
    
    * update doc
    
    * add spanner example with httpserver
    
    * update example
    
    * update spanner dependency in pharaoh
    
    * update changelog

--- packages/spanner/README.md
+++ packages/spanner/README.md
@@ -3,15 +3,33 @@
 Generic HTTP Router implementation, internally uses a Radix Tree (aka compact Prefix Tree), supports route params, wildcards.
 
 ```dart
-    test('with fixed suffix', () async {
-      final router = Spanner()
-        ..addRoute(HTTPMethod.GET, '/user', null)
-        ..addRoute(HTTPMethod.GET, '/user/<userId>', null)
-        ..addRoute(HTTPMethod.GET, '/user/<userId>/details', null)
-        ..addRoute(HTTPMethod.GET, '/user/<file>.png/download', null)
-        ..addRoute(HTTPMethod.GET, '/user/<file>.png/<user2>/hello', null)
-        ..addRoute(HTTPMethod.GET, '/a/<param>-static', null)
-        ..addRoute(HTTPMethod.GET, '/b/<param>.static', null);
+import 'package:spanner/spanner.dart';
 
-    });
+void main() {
+  final spanner = Spanner();
+
+  List<String> getUsers() => ['Foo', 'Bar'];
+
+  String getUser(String userId) => 'Hello $userId';
+
+  spanner.addRoute(HTTPMethod.GET, '/', getUsers);
+
+  spanner.addRoute(HTTPMethod.GET, '/<userId>', getUser);
+
+  final result = spanner.lookup(HTTPMethod.GET, '/');
+  if (result == null) return;
+  
+
+  /// This contains all parameters that were resolved in the route
+  final routeParams = result.params; // Map<String, dynamic>
+
+  /// your handler will be in this list.
+  ///
+  /// If any middlewares where resolved along the route to this handler
+  /// they'll be present in the list
+  ///
+  /// The list is ordered in the exact way you registed your middlewares and handlers
+  final resolvedHandler = result.values; // List<dynamic>
+}
+
 ```
--- packages/spanner/example/spanner_example.dart
+++ packages/spanner/example/spanner_example.dart
@@ -1,1 +1,55 @@
-void main() {}
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:spanner/spanner.dart';
+
+typedef Handler = String Function(Map<String, dynamic> params);
+
+void main() async {
+  final spanner = Spanner();
+
+  getUsers(Map<String, dynamic> params) => jsonEncode(['Foo', 'Bar']);
+
+  getUser(Map<String, dynamic> params) => 'Hello ${params['userId']}';
+
+  spanner
+    ..addRoute(HTTPMethod.GET, '/', getUsers)
+    ..addRoute(HTTPMethod.GET, '/<userId>', getUser);
+
+  final server = await HttpServer.bind('localhost', 0);
+
+  print('Server Started on port: ${server.port}');
+
+  server.listen((request) {
+    if (request.method != 'GET') {
+      request.response
+        ..write('Request not supported')
+        ..close();
+      return;
+    }
+
+    final result = spanner.lookup(HTTPMethod.GET, request.uri.path);
+    if (result == null) {
+      request.response
+        ..write('Request not supported')
+        ..close();
+      return;
+    }
+
+    final params = result.params; // Map<String, dynamic>
+
+    /// your handler will be in this list.
+    ///
+    /// If any middlewares where resolved along the route to this handler
+    /// they'll be present in the list
+    ///
+    /// The list is ordered in the exact way you registed your middlewares
+    /// and handlers
+    final resolvedHandler = result.values; // List<dynamic>
+
+    final handlerResult = (resolvedHandler.first as Handler).call(params);
+    request.response
+      ..write(handlerResult)
+      ..close();
+  });
+}
